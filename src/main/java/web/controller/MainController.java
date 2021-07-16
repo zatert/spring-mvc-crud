f7fa7b0d07bbe8fcd5b81c23cf1133bbba853d6e
User <izatert@gmail.com>
2021-07-16 12:19:37 +0300

    3.0 version

--- src/main/java/web/controller/MainController.java
+++ src/main/java/web/controller/MainController.java
@@ -8,15 +8,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import web.dao.UserDaoImpl;
 import web.model.User;
+import web.repository.UserRepo;
+import web.service.UserService;
+
+import java.util.List;
 
 @Controller
 //@RequestMapping("/users")
 public class MainController {
-
-    @RequestMapping("/users")
-    public String pageForRest(){
-        return "restform";
-    }
+    @Autowired
+    private UserDaoImpl userDao;
+//    @Autowired
+//    private UserRepo userRepo;
+//    @RequestMapping("/users")
+//    public String pageForRest(){
+//        return "restform";
+//    }
 //    @Autowired
 //    private UserDaoImpl userDao;
 //
@@ -27,12 +34,47 @@ public class MainController {
 //        return "Hello, i'm controller";
 //    }
 //
-//    @GetMapping(value = "/users")
-//    public String allUsers(Model model){
-//        //List<User> list = userDao.all();
-//        model.addAttribute("list", userDao.all());
-//        return "users";
-//    }
+    @GetMapping(value = "/users")
+    public String allUsers(Model model){
+        List<User> list = userDao.all();
+        model.addAttribute("list", list);
+        //return "redirect:/users";
+        return "users";
+    }
+
+    @GetMapping("/newUserForm")
+    public String newUserForm(Model model) {
+        // create model attribute to bind form data
+        User user = new User();
+        model.addAttribute("user", user);
+        return "new_user";
+    }
+    @PostMapping("/saveUser")
+    public String saveUser(@ModelAttribute("user") User user){
+        userDao.addUser(user);
+        //userRepo.save(user);
+        return "redirect:/users";
+    }
+    @PostMapping("/updateUser")
+    public String updateUser(@ModelAttribute("user") User user){
+        userDao.edit(user);
+        //userRepo.save(user);
+        return "redirect:/users";
+    }
+    @GetMapping("/formForUpdate/{id}")
+    public String formForUpdate(@PathVariable(value = "id")Integer id, Model model){
+//        User user = userRepo.findById(id).get();
+        User user = userDao.getOne(id);
+        // set employee as a model attribute to pre-populate the form
+        model.addAttribute("user", user);
+        return "update_user";
+    }
+    @GetMapping("/deleteUser/{id}")
+    public String deleteUser(@PathVariable(value = "id")Integer id){
+//        userRepo.deleteById(id);
+        userDao.delete(id);
+        return "redirect:/users";
+    }
 //
 //    @PostMapping(value="/add")
 //    public String add(@RequestParam String name, @RequestParam String lastname, @RequestParam Integer age,
